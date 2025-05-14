4507302d2588cc51898801f613bbf39c02eed61b
HaoNguyen <haoytp@gmail.com>
2025-05-14 07:38:52 +0700

    first commit

---
+++ TaskManagerBackEnd/src/main/java/com/example/taskmanagerbackend/TaskManagerBackEndApplication.java
@@ -0,0 +1,12 @@
+package com.example.taskmanagerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagerBackEndApplication.class, args);
+    }
+}
